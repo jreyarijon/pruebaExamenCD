73c7535bcb261a2b53154712c9847fd31e3e36e3
unknown <jreyarijon@danielcastelao.org>
2020-01-22 13:35:47 +0100

    B

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -17,6 +17,7 @@ public class ExCOD1Av {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("1ª modificacion");
+        System.out.println("2ª modificacion");
     }
     
 }
